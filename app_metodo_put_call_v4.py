7a417cc82aecb179c3948bf48b9b68ad0cc5dd25
materialracional <materialracional@gmail.com>
2026-07-15 10:36:10 -0300

    Add files via upload

--- app_metodo_put_call_v4.py
+++ app_metodo_put_call_v4.py
@@ -97,11 +97,13 @@ def criar_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-pipe")
 
-    service = Service("/usr/bin/chromedriver")
+    service = Service(
+        executable_path="/usr/bin/chromedriver"
+    )
 
     return webdriver.Chrome(
         service=service,
